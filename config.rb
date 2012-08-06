14b4efb9e97d72553a7bd77fa2a071dfd1c9fb10
Bent Cardan <bent@kickr.io>
2012-08-06 13:18:06 -0400

    adding backbone.js

--- config.rb
+++ config.rb
@@ -5,7 +5,7 @@ project_type = :stand_alone
 http_path = "/"
 http_images_path = "/images"
 http_fonts_path = "/fonts"
-css_dir = "_site/stylesheets"
+css_dir = "/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
